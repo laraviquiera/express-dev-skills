49004ecdb5ca8e77f587db28d733561591520aee
username <lara.viquiera@gmail.com>
2024-01-04 15:43:08 -0800

    required method override

--- routes/skills.js
+++ routes/skills.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const skillsCtrl = require('../controllers/skills');
 
-
+//GET /skills
 router.get('/', skillsCtrl.index);
+//GET /skills/new
+router.get('/new', skillsCtrl.new);
+//GET /skills/:id
 router.get('/:id', skillsCtrl.show);
+//GET /skills/new
+
 
 module.exports = router;
